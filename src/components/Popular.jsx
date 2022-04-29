406337286c2d2efec4a0249d7b68fe035a6d5e93
najeesimmons <najee.bryant.simmons@gmail.com>
2022-04-29 10:21:18 -0400

    rudimentary Popular component constructed

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,33 @@
-
+import { useEffect, useState} from "react";
 
 function Popular() {
-  return (
-    <div>Popular</div>
-  )
+
+   const [popular, setPopular] = useState([]); 
+  // useEffect will run the getPopular function as soon as  component is mounted  
+
+  useEffect(() => {
+      getPopular();
+  // in square brackets below, a condition can be added which re-triggers useEffect/
+  // getPopular    
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    setPopular(data.recipes)
+  };
+
+  return <div>
+      {popular.map(recipe => {
+          return (
+              <div key={recipe.id}>
+                  {recipe.title}
+            </div>
+          )
+      })}
+  </div>;
 }
 
-export default Popular
+export default Popular;
